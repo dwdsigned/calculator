c510618813a1b5fe6c1edfd14147942cbce1c7dd
Darius Williams <dariustwilliams668@gmail.com>
2024-01-02 17:30:13 -0500

    grid area for operator buttons

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 const numbersDiv = document.getElementById("numberButtons");
 let digits = document.querySelectorAll(".numbers");
+let operators = document.querySelectorAll(".operator")
 // console.log(numberButtons);
 // console.log(numberButtons.length);
 
-
+// operator
 
 // Using a For Of loop, assigns the grid-area to the button's id
 for (button of digits) {
+    button.style.gridArea = `${button.id}`;
+}
+
+// Using a For Of loop, assigns the grid-area to the button's id for the operators
+for (button of operators) {
     button.style.gridArea = `${button.id}`;
 }
