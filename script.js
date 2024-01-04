0951083bbd4eac1c0a2fcb8246b482ad75551f0b
Darius Williams <dariustwilliams668@gmail.com>
2024-01-04 16:26:50 -0500

    getting started with functionality

--- script.js
+++ script.js
@@ -15,3 +15,26 @@ for (button of digits) {
 for (button of operators) {
     button.style.gridArea = `${button.id}`;
 }
+
+
+
+
+// Calculator functionality
+// What do I need?
+// Each button should enter the corresponding number or operator when pressed 
+// The display needs to clear when the "C" button is pressed. The display should be reset to "0" "Zero"
+
+// *************************************************************************************************************
+
+// Store first number
+// operator
+// +,-, *, / operators should put first/previous number somewhere
+// = operator should add all previous numbers together
+// display = [1,]
+
+const calculatorDisplay = document.getElementById("calculatorDisplay");
+console.log(calculatorDisplay);
+
+const enteredNumbers = [533, 34, 243, 34];
+
+calculatorDisplay.textContent = `${enteredNumbers[0]} ${enteredNumbers[1]} ${enteredNumbers[2]} ${enteredNumbers[3]}`;
